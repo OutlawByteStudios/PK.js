a8687f39352799f9716271097e7c702dc7a597ce
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-07-31 22:44:07 +0100

    Admin Permission Bug Fix

--- client/src/graphql/mutations/admin-management/update-admin-permission.js
+++ client/src/graphql/mutations/admin-management/update-admin-permission.js
@@ -2,62 +2,36 @@ import { gql } from 'apollo-boost';
 
 import Fragments from '../../fragments/index';
 
+import { panelPermissions, gamePermissions } from 'shared/constants';
+
+const addPermissionsVars = () => {
+  let permissions = '';
+  for (let permission of panelPermissions.concat(gamePermissions)) {
+    permissions += `$${permission.permission}: Int\n`;
+  }
+  return permissions;
+};
+
+const addPermissionsParams = () => {
+  let permissions = '';
+  for (let permission of panelPermissions.concat(gamePermissions)) {
+    permissions += `${permission.permission}: $${permission.permission}\n`;
+  }
+  return permissions;
+};
+
 export default gql`
   mutation UpdateAdminPermission(
     $serverID: Int!
     $steamID: String!
     $guid: String
-    $manageAssignPermissions: Int
-    $viewAdminPermissions: Int
-    $adminTools: Int
-    $adminPanel: Int
-    $adminMute: Int
-    $adminKick: Int
-    $adminTemporaryBan: Int
-    $adminPermanentBan: Int
-    $adminKillFade: Int
-    $adminFreeze: Int
-    $adminSpectate: Int
-    $adminTeleport: Int
-    $adminHealSelf: Int
-    $adminGodlike: Int
-    $adminJoinFactions: Int
-    $adminAnnouncements: Int
-    $adminPolls: Int
-    $adminShips: Int
-    $adminGold: Int
-    $adminItems: Int
-    $adminAllItems: Int
-    $adminFactions: Int
-    $adminAnimals: Int
+    ${addPermissionsVars()}
 ){
   updateAdminPermission(
     serverID: $serverID
     steamID: $steamID
     guid: $guid
-    manageAssignPermissions: $manageAssignPermissions
-    viewAdminPermissions: $viewAdminPermissions
-    adminTools: $adminTools
-    adminPanel: $adminPanel
-    adminMute: $adminMute
-    adminKick: $adminKick
-    adminTemporaryBan: $adminTemporaryBan
-    adminPermanentBan: $adminPermanentBan
-    adminKillFade: $adminKillFade
-    adminFreeze: $adminFreeze
-    adminSpectate: $adminSpectate
-    adminTeleport: $adminTeleport
-    adminHealSelf: $adminHealSelf
-    adminGodlike: $adminGodlike
-    adminJoinFactions: $adminJoinFactions
-    adminAnnouncements: $adminAnnouncements
-    adminPolls: $adminPolls
-    adminShips: $adminShips
-    adminGold: $adminGold
-    adminItems: $adminItems
-    adminAllItems: $adminAllItems
-    adminFactions: $adminFactions
-    adminAnimals: $adminAnimals
+    ${addPermissionsParams()}
   ) {
     admin {
       _id
