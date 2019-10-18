5ff3018278b1ad9a24f253ea6094f1a77a26eb70
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-10-19 00:54:37 +0100

    Sort Admins by Perm Level

--- server/src/graphql-api/resolvers/queries/admin-permission.js
+++ server/src/graphql-api/resolvers/queries/admin-permission.js
@@ -1,4 +1,21 @@
 import { AdminPermission } from '../../../models';
+import { panelPermissions, gamePermissions } from 'shared/constants';
+
+const sortAdmins = (admins) => {
+  admins = admins.map(admin => {
+    admin = admin.toObject();
+
+    let totalPermissions = 0;
+    for(let permission of panelPermissions.concat(gamePermissions)) totalPermissions += admin[permission.permission];
+
+    return {
+      ...admin,
+      totalPermissions
+    }
+  }).sort((a, b) => { return b.totalPermissions - a.totalPermissions });
+
+  return admins;
+} ;
 
 export default {
   Query: {
@@ -10,9 +27,11 @@ export default {
     },
 
     adminPermissions: async (parent, filter) => {
-      return AdminPermission.find({
+      let admins = await AdminPermission.find({
         $or: [{ server: filter.serverID }, { admin: filter.steamID }]
       });
+
+      return sortAdmins(admins);
     }
   },
 
@@ -25,9 +44,11 @@ export default {
     },
 
     adminPermissions: async parent => {
-      return AdminPermission.find({
+      let admins = await AdminPermission.find({
         server: parent.id
       });
+
+      return sortAdmins(admins);
     }
   }
 };
